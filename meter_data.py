fe8d130c70e0351795fc5bf4ad10fcc5c28b7b09
Cassie Huber <chuber@hawaii.edu>
2025-12-19 12:58:42 -1000

    Update meter_data.py

--- meter_data.py
+++ meter_data.py
@@ -19,4 +19,19 @@ def get_csv_paths(base_path):
     """
     csv_paths = []
     
+    for subfolder in os.listdir(base_path):
+        # create path for each subfolder
+        folder_path = os.path.join(base_path, subfolder)
+
+        # get the name of the meter from the subfolder name
+        meter_name = subfolder.lower().replace(' ', '_').replace('_mtr', '')
+
+        # list of csv file paths in subfolder
+        # ignore hiddent '._' files on macOS
+        csv_paths = csv_paths.append([os.path.join(folder_path, f)
+                     for f in os.listdir(folder_path)
+                     if f.endswith('.csv')
+                     #and not f.startswith('._')
+                     and not f.startswitch('.')
+                     ])
 
